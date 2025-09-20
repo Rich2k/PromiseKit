0230bb96e9119dacd6b69469e0e1fde82707b08b
Richard Hyland <rhyland@gmail.com>
2025-09-20 23:25:21 +0100

    Fix shims

--- Package.swift
+++ Package.swift
@@ -1,22 +1,40 @@
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
     name: "PromiseKit",
-    exclude: [
-        "Sources/AnyPromise.swift",
-        "Sources/Promise+AnyPromise.swift",
-        "Sources/AnyPromise.m",
-        "Sources/dispatch_promise.m",
-        "Sources/GlobalState.m",
-        "Sources/hang.m",
-        "Sources/NSMethodSignatureForBlock.m",
-        "Sources/join.m",
-        "Sources/PMKCallVariadicBlock.m",
-        "Sources/when.m",
-        "Sources/after.m",
-        "Sources/AnyPromise+Private.h",
-        "Sources/AnyPromise.h",
-        "Sources/PromiseKit.h",
-        "Tests"
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15),
+        .tvOS(.v12),
+        .watchOS(.v5)
+    ],
+    products: [
+        .library(
+            name: "PromiseKit",
+            targets: ["PromiseKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "PromiseKit",
+            path: "Sources",
+            exclude: [
+                "AnyPromise.swift",
+                "Promise+AnyPromise.swift",
+                "AnyPromise.m",
+                "dispatch_promise.m",
+                "GlobalState.m",
+                "hang.m",
+                "NSMethodSignatureForBlock.m",
+                "join.m",
+                "PMKCallVariadicBlock.m",
+                "when.m",
+                "after.m",
+                "AnyPromise+Private.h",
+                "AnyPromise.h",
+                "PromiseKit.h"
+            ]
+        )
     ]
 )
---
+++ Swift4_2_Shims.swift
@@ -0,0 +1,10 @@
+#if swift(>=5.0)
+import UIKit
+
+public typealias UIAlertControllerStyle = UIAlertController.Style
+public typealias UIAlertActionStyle = UIAlertAction.Style
+public typealias UIControlState = UIControl.State
+public typealias UIControlEvents = UIControl.Event
+public typealias UIViewAnimationOptions = UIView.AnimationOptions
+// add others as needed based on the errors you see
+#endif
